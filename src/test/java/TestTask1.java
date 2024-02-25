01615d83b43dcb7a3337bee1e8c968b1993915ee
Vladyka <kuchikiruy@gmail.com>
2024-02-25 15:38:47 +0300

    тестики

--- src/test/java/TestTask1.java
+++ src/test/java/TestTask1.java
@@ -1,9 +1,37 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import ru.study.account.Account;
 
 public class TestTask1 {
 
-    public void createAccount(){
-        Account acc = new Account("Alex");
 
+    Account acc = new Account("Alex");
+    @Test
+    @DisplayName("Создаем аккаунты")
+    public void testCreateAccount(){
+        acc.setName("IVAN");
+        try {
+            Account acc2 = new Account(null);
+            Account acc3 = new Account("");
+        } catch (IllegalArgumentException e) {
+            System.out.println("норм");
+        }
+
+
+    }
+    @Test
+    public void testMoney() {
+        acc.updateMoney("RUR", 50);
+        acc.updateMoney("EUR", 50);
+        System.out.println(acc.getMoney());
+        acc.updateMoney("RUR", 100);
+        System.out.println(acc.getMoney("RUR"));
+        try {
+            acc.updateMoney("RUR", -10);
+            acc.updateMoney("", 50);
+        } catch (IllegalArgumentException e) {
+            System.out.println("нормsss");
+        }
     }
 }
